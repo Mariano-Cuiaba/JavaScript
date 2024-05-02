e628026f2661f68379ca2a301f576ebef9ca270a
Mariano-Cuiaba <educlanmariano@gmail.com>
2024-05-02 16:26:45 -0400

    atividade do JavaScript.js

---
+++ parseInt.js
@@ -0,0 +1,8 @@
+let stringParaNumero = "100";
+console.log(stringParaNumero); 
+console.log(typeof(stringParaNumero));
+console.log("");
+
+let numero = parseInt(stringParaNumero);
+console.log(numero);
+console.log(typeof(numero));
