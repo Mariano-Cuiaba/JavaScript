24063ee8d7ad2cbfe00e6474ed5222e2522c09f6
Mariano-Cuiaba <educlanmariano@gmail.com>
2024-05-02 16:35:10 -0400

    atividade do JavaScript.js

---
+++ parseFloat.js
@@ -0,0 +1,8 @@
+let stringParaDecimal = "12.60";
+console.log(stringParaDecimal); 
+console.log(typeof(stringParaDecimal));
+console.log("");
+
+let numero = parseInt(stringParaDecimal);
+console.log(decimal);
+console.log(typeof(decimal));
