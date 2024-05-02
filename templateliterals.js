8288508c43663af19c0e9c557f098c151025ddb3
Mariano-Cuiaba <educlanmariano@gmail.com>
2024-05-02 17:06:32 -0400

    atividade do JavaScript.js

---
+++ templateliterals.js
@@ -0,0 +1,6 @@
+let nome = "Wanderson Timóteo";
+let idade = 30;
+
+// Usando template literals para crias uma string com variáveis
+let saudacao = `Olá, meu nome é ${nome} e eu tenho ${idade} anos.`;
+console.log(saudacao); //m Saída: "Olá, meu nome é Wanderson Timóteo e eu tenho 30 anos."
