3e79762003d4b51e391ab7261b0c15028bb77933
Mariano-Cuiaba <educlanmariano@gmail.com>
2024-05-07 17:18:55 -0400

    atividades operadores.js

---
+++ Operadores/operador-ternario-04.js
@@ -0,0 +1,4 @@
+const firstCheck = false;
+const secondCheck = false;
+const access = firstCheck ? "Acesso negado" : secondCheck ? "Acesso negado" : "Acesso concedido";
+console.log(access);0 // Saída: "Acessp concedido"
